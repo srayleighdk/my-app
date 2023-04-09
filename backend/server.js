e223f72891c6123c9e5756ccdbfbe45ff5f241d9
kali <kali@kalipc>
2023-04-09 08:23:50 -0400

    add backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// Import the necessary dependencies
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config;
+
+
+// Create anew instance of the Express application
+const app = express();
+
+// Use CORS middleware to enable Cross-Origin Resource Sharing
+app.use(cors());
+
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+// Start the server on the specified port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
